9297b5f87eab654031b7fb3ab80eba95eee4c641
Jonathan Rowlands <jonrowlands83@gmail.com>
2014-02-13 16:02:41 -0500

    Wrapped in umd

--- build/coffeescript-namespace.js
+++ build/coffeescript-namespace.js
@@ -1,18 +1,33 @@
 (function() {
   var __slice = [].slice;
 
-  this.namespace = function(target, name, block) {
-    var item, top, _i, _len, _ref, _ref1;
-    if (arguments.length < 3) {
-      _ref = [(typeof exports !== 'undefined' ? exports : window)].concat(__slice.call(arguments)), target = _ref[0], name = _ref[1], block = _ref[2];
+  (function(root, factory) {
+    if (typeof exports === "object" && exports) {
+      module.exports = factory();
+    } else if (typeof define === "function" && define.amd) {
+      define(factory);
+    } else {
+      root.coffeescriptNamespace = factory();
     }
-    top = target;
-    _ref1 = name.split('.');
-    for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
-      item = _ref1[_i];
-      target = target[item] || (target[item] = {});
-    }
-    return block(target, top);
-  };
+  })(this, (function() {
+    var exports;
+    exports = {};
+    exports.name = "coffeescript-namespace";
+    exports.version = "0.1.0";
+    exports.namespace = function(target, name, block) {
+      var item, top, _i, _len, _ref, _ref1;
+      if (arguments.length < 3) {
+        _ref = [(typeof exports !== 'undefined' ? exports : window)].concat(__slice.call(arguments)), target = _ref[0], name = _ref[1], block = _ref[2];
+      }
+      top = target;
+      _ref1 = name.split('.');
+      for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
+        item = _ref1[_i];
+        target = target[item] || (target[item] = {});
+      }
+      return block(target, top);
+    };
+    return exports;
+  }));
 
 }).call(this);
--- src/main/coffeescript-namespace.coffee
+++ src/main/coffeescript-namespace.coffee
@@ -1,5 +1,21 @@
-@namespace = (target, name, block) ->
-  [target, name, block] = [(if typeof exports isnt 'undefined' then exports else window), arguments...] if arguments.length < 3
-  top    = target
-  target = target[item] or= {} for item in name.split '.'
-  block target, top
+((root, factory) ->
+  if typeof exports is "object" and exports
+    module.exports = factory() # CommonJS
+  else if typeof define is "function" and define.amd
+    define factory # AMD (RequireJS and family)
+  else
+    root.coffeescriptNamespace = factory() # Browser <script>
+  return
+) this, (->
+  exports = {}
+  exports.name = "coffeescript-namespace"
+  exports.version = "0.1.0"
+  exports.namespace = (target, name, block) ->
+    [target, name, block] = [(if typeof exports isnt 'undefined' then exports else window), arguments...] if arguments.length < 3
+    top    = target
+    target = target[item] or= {} for item in name.split '.'
+    block target, top
+
+  exports
+)
+
